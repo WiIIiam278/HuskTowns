d4b1dcce479beb2004285a989232d852f5a19241
WiIIiam278 <will27528@gmail.com>
2021-05-10 23:12:12 +0100

    definalised events
    Signed-off-by: WiIIiam278 <will27528@gmail.com>

--- src/main/java/me/william278/husktowns/integration/HuskHomes.java
+++ src/main/java/me/william278/husktowns/integration/HuskHomes.java
@@ -1,13 +1,20 @@
 package me.william278.husktowns.integration;
 
 import me.william278.huskhomes2.api.HuskHomesAPI;
+import me.william278.huskhomes2.api.events.PlayerHomeUpdateEvent;
+import me.william278.huskhomes2.api.events.PlayerSetHomeEvent;
 import me.william278.husktowns.HuskTowns;
+import me.william278.husktowns.MessageManager;
+import me.william278.husktowns.object.chunk.ClaimedChunk;
 import me.william278.husktowns.object.teleport.TeleportationPoint;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 
-public class HuskHomes {
+public class HuskHomes implements Listener {
 
     private static HuskHomesAPI huskHomesAPI;
     private static final HuskTowns plugin = HuskTowns.getInstance();
@@ -33,6 +40,46 @@ public class HuskHomes {
 
     public static void queueTeleport(Player player, TeleportationPoint point) {
         huskHomesAPI.teleportPlayer(player, point.toHuskHomes(), true);
+    }
+
+    @EventHandler
+    public void onPlayerSetHome(PlayerSetHomeEvent e) {
+        Location location = e.getHome().getLocation();
+        String playerTown = HuskTowns.getPlayerCache().getTown(e.getPlayer().getUniqueId());
+        ClaimedChunk chunk = HuskTowns.getClaimCache().getChunkAt(location.getChunk().getX(),
+                location.getChunk().getZ(), location.getWorld().getName());
+
+        if (chunk != null) {
+            if (playerTown != null) {
+                if (!chunk.getTown().equals(playerTown)) {
+                    MessageManager.sendMessage(e.getPlayer(), "error_cannot_sethome", chunk.getTown());
+                    e.setCancelled(true);
+                }
+            } else {
+                MessageManager.sendMessage(e.getPlayer(), "error_cannot_sethome", chunk.getTown());
+                e.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void updateHomeLocation(PlayerHomeUpdateEvent e) {
+        Location location = e.getHome().getLocation();
+        String playerTown = HuskTowns.getPlayerCache().getTown(e.getPlayer().getUniqueId());
+        ClaimedChunk chunk = HuskTowns.getClaimCache().getChunkAt(location.getChunk().getX(),
+                location.getChunk().getZ(), location.getWorld().getName());
+
+        if (chunk != null) {
+            if (playerTown != null) {
+                if (!chunk.getTown().equals(playerTown)) {
+                    MessageManager.sendMessage(e.getPlayer(), "error_cannot_update_sethome", chunk.getTown());
+                    e.setCancelled(true);
+                }
+            } else {
+                MessageManager.sendMessage(e.getPlayer(), "error_cannot_update_sethome", chunk.getTown());
+                e.setCancelled(true);
+            }
+        }
     }
 
 }
