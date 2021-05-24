bef4e335dbcc1233cec702b6849d40d7eb9d0881
BuildTools <unconfigured@null.spigotmc.org>
2021-05-24 16:22:42 +0100

    More work on BlueMap integration

--- src/main/java/me/william278/husktowns/integration/BlueMap.java
+++ src/main/java/me/william278/husktowns/integration/BlueMap.java
@@ -1,7 +1,12 @@
 package me.william278.husktowns.integration;
 
+import de.bluecolored.bluemap.api.BlueMapAPI;
+import de.bluecolored.bluemap.api.marker.MarkerSet;
 import me.william278.husktowns.HuskTowns;
 import org.bukkit.plugin.Plugin;
+
+import java.io.IOException;
+import java.util.Optional;
 
 public class BlueMap {
 
@@ -9,6 +14,24 @@ public class BlueMap {
     private static final HuskTowns plugin = HuskTowns.getInstance();
 
    // todo https://github.com/BlueMap-Minecraft/BlueMapAPI/wiki
+
+    /*public static Optional<MarkerSet> getMarkerSet() {
+        BlueMapAPI.getInstance().ifPresent(api -> {
+            //code executed when the api is enabled (skipped if the api is not enabled)
+            try {
+                Optional<MarkerSet> markerSet;
+                markerSet = api.getMarkerAPI().getMarkerSet("Towns");
+                if (markerSet.isPresent()) {
+                    return markerSet;
+                } else {
+                    return api.getMarkerAPI().createMarkerSet("Towns");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        return Optional.empty();
+    }*/
 
     public static void initialize() {
         if (HuskTowns.getSettings().doBlueMap()) {
