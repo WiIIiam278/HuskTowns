899ff6836e608c0a78da91bd56ef544d7782a3b3
William <will27528@gmail.com>
2023-01-06 19:57:44 +0000

    HuskHomes hook: Prevent setting home in town claims without access

--- bukkit/src/main/java/net/william278/husktowns/hook/HuskHomesHook.java
+++ bukkit/src/main/java/net/william278/husktowns/hook/HuskHomesHook.java
@@ -1,20 +1,26 @@
 package net.william278.husktowns.hook;
 
 import net.william278.huskhomes.api.HuskHomesAPI;
+import net.william278.huskhomes.event.HomeSaveEvent;
 import net.william278.huskhomes.position.Server;
-import net.william278.huskhomes.position.World;
 import net.william278.huskhomes.teleport.TimedTeleport;
+import net.william278.husktowns.BukkitHuskTowns;
 import net.william278.husktowns.HuskTowns;
 import net.william278.husktowns.claim.Position;
+import net.william278.husktowns.claim.World;
+import net.william278.husktowns.listener.Operation;
 import net.william278.husktowns.user.BukkitUser;
 import net.william278.husktowns.user.OnlineUser;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
 import java.util.logging.Level;
 
-public class HuskHomesHook extends TeleportationHook {
+public class HuskHomesHook extends TeleportationHook implements Listener {
     @Nullable
     private HuskHomesAPI api;
 
@@ -25,6 +31,7 @@ public class HuskHomesHook extends TeleportationHook {
     @Override
     public void onEnable() {
         this.api = HuskHomesAPI.getInstance();
+        Bukkit.getPluginManager().registerEvents(this, (BukkitHuskTowns) plugin);
         plugin.log(Level.INFO, "Enabled HuskHomes teleportation hook");
     }
 
@@ -37,14 +44,32 @@ public class HuskHomesHook extends TeleportationHook {
                         position.getZ(),
                         position.getYaw(),
                         position.getPitch(),
-                        new World(position.getWorld().getName(), position.getWorld().getUuid()),
+                        new net.william278.huskhomes.position.World(position.getWorld().getName(), position.getWorld().getUuid()),
                         new Server(server)
                 ))
                 .toTimedTeleport()
                 .thenAccept(TimedTeleport::execute));
     }
 
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerSetHome(@NotNull HomeSaveEvent event) {
+        final Optional<? extends OnlineUser> user = plugin.getOnlineUsers().stream()
+                .filter(online -> online.getUuid().equals(event.getHome().owner.uuid)).findFirst();
+        if (user.isEmpty()) {
+            return;
+        }
+
+        final Position position = Position.at(event.getHome().x, event.getHome().y, event.getHome().z,
+                World.of(event.getHome().world.uuid, event.getHome().world.name,
+                        event.getHome().world.environment.name().toLowerCase()));
+        if (plugin.getOperationHandler().cancelOperation(Operation
+                .of(user.get(), Operation.Type.BLOCK_INTERACT, position))) {
+            event.setCancelled(true);
+        }
+    }
+
     private Optional<HuskHomesAPI> getHuskHomes() {
         return Optional.ofNullable(api);
     }
+
 }
