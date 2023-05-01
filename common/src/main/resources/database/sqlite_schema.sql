5b2eb6e805783d4910fe9368005813382ca60221
William <will27528@gmail.com>
2023-05-01 17:05:58 +0100

    Account for longer world environment names in some server implementations

--- common/src/main/resources/database/sqlite_schema.sql
+++ common/src/main/resources/database/sqlite_schema.sql
@@ -23,6 +23,6 @@ CREATE TABLE IF NOT EXISTS `%claim_data%`
     `server_name`       varchar(255) NOT NULL,
     `world_uuid`        char(36)     NOT NULL,
     `world_name`        varchar(128) NOT NULL,
-    `world_environment` varchar(16)  NOT NULL,
+    `world_environment` varchar(32)  NOT NULL,
     `claims`            longblob     NOT NULL
 );
