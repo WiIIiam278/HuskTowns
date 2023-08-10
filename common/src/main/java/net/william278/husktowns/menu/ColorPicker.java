2938e37fc53993de0383309bf91d4654a4b895c6
William <will27528@gmail.com>
2023-08-10 17:54:23 +0100

    v2.5.3: Bug fixes, deprecate usage of java.awt.Color, new regex validation options (#306)
    * Bump annotaml, use paperlib for command reg
    
    * Reduce unnecessarily large test count
    
    * Deprecate use of awt.Color, use adventure TextColor, close #304
    
    * Cleanup leftover awt usage
    
    * Change color picker swatch to `|` (pipe)
    
    * Add ability to customize town name/meta regex, close #305
    
    * Update config.yml comments and docs
    
    * Update default flag enabled character to `☑`
    
    * Fixup spawn reason checking logic, close #283
    
    * Remove commands from plugin.yml
    
    * tests: Increase logout time for `testTownPruningWithMultipleInactiveMembers`
    
    * tests: Improve assertion rigidity for prune checking

--- common/src/main/java/net/william278/husktowns/menu/ColorPicker.java
+++ common/src/main/java/net/william278/husktowns/menu/ColorPicker.java
@@ -22,13 +22,13 @@ package net.william278.husktowns.menu;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.util.HSVLike;
 import org.jetbrains.annotations.NotNull;
-
-import java.awt.*;
 
 /**
  * Utility for displaying an interactive color picker in chat
  */
+@SuppressWarnings("unused")
 public class ColorPicker {
 
     private final int width;
@@ -82,10 +82,11 @@ public class ColorPicker {
      */
     @NotNull
     private String getColorAt(int x, int y) {
-        final Color color = Color.getHSBColor(
-                (float) x / width, (float) y / height,
-                (y == 0 ? (float) x / width : 1f));
-        return String.format("#%02x%02x%02x", color.getRed(), color.getGreen(), color.getBlue());
+        return TextColor.color(HSVLike.hsvLike(
+                (float) x / width,
+                (float) y / height,
+                (y == 0 ? (float) x / width : 1f)
+        )).asHexString();
     }
 
     /**
@@ -106,7 +107,7 @@ public class ColorPicker {
         private int width = 90;
         private int height = 6;
         private String command;
-        private char swatch = '▏';
+        private char swatch = '|';
 
         /**
          * Set the width (number of columns) of the color picker
