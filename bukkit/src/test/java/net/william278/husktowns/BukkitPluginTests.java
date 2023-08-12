3341cca42c95e6a12b6d74daf4acc6b34bee52dc
William <will27528@gmail.com>
2023-08-12 14:37:35 +0100

    tests: Run with `--stacktrace` arg to debug flaking tests
    (cherry picked from commit 9962a3c5365933acad89272966d36ca370e574aa)

--- bukkit/src/test/java/net/william278/husktowns/BukkitPluginTests.java
+++ bukkit/src/test/java/net/william278/husktowns/BukkitPluginTests.java
@@ -118,7 +118,6 @@ public class BukkitPluginTests {
     @Nested
     @DisplayName("Town Pruning Tests")
     @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-    @TestInstance(TestInstance.Lifecycle.PER_METHOD)
     public class PruningTests {
 
         private static final Map<String, Long> TEST_DATA = Map.of(
