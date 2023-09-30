df9c9decb26fbb464e1c5b445877b69e0bf0047c
William <will27528@gmail.com>
2023-09-30 17:28:04 +0100

    Code cleanup

--- common/src/main/java/net/william278/husktowns/hook/TeleportationHook.java
+++ common/src/main/java/net/william278/husktowns/hook/TeleportationHook.java
@@ -37,6 +37,8 @@ public abstract class TeleportationHook extends Hook {
      *
      * @param user     the user to teleport
      * @param position the position to teleport to
+     * @param server   the server to teleport to
+     * @param instant  whether to teleport instantly
      */
     public abstract void teleport(@NotNull OnlineUser user, @NotNull Position position, @NotNull String server,
                                   boolean instant);
