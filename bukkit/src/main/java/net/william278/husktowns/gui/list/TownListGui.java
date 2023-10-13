bd68f26b105fb41db70b33b52fd9f1dd1f7e1d7a
Emibergo02 <36164338+Emibergo02@users.noreply.github.com>
2023-10-13 12:57:06 +0200

    Removed unnecessary dependencies

--- bukkit/src/main/java/net/william278/husktowns/gui/list/TownListGui.java
+++ bukkit/src/main/java/net/william278/husktowns/gui/list/TownListGui.java
@@ -103,8 +103,8 @@ public class TownListGui extends PagedItemsGuiAbstract {
             @Override
             public ItemProvider getItemProvider(PagedGui<?> gui) {
                 return forward ?
-                        guiSettings.getItem("forwardButton").toItemProvider() :
-                        guiSettings.getItem("backButton").toItemProvider();
+                        guiSettings.getItem("forwardItem").toItemProvider() :
+                        guiSettings.getItem("backItem").toItemProvider();
             }
         };
     }
