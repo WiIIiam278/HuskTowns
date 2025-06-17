b568513ddee319420483620c42013d39c70dd59d
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2025-06-17 18:57:39 -0500

    accurate javadoc

--- common/src/main/java/net/william278/husktowns/events/ITownUpdateEvent.java
+++ common/src/main/java/net/william278/husktowns/events/ITownUpdateEvent.java
@@ -1,7 +1,9 @@
 package net.william278.husktowns.events;
 
+import net.william278.husktowns.HuskTowns;
+
 /**
- * An event fired when a town is edited by a player
+ * An event fired when a town is {@link HuskTowns#updateTown updated}
  */
 public interface ITownUpdateEvent extends TownEvent {
 
